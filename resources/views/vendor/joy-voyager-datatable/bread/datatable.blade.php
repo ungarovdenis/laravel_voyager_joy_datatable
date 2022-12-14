1020090833dc908a1d7ad4d362a4e4e5b96fb0de
denisungarov <denis.ungaroff@gmail.com>
2022-12-15 01:02:46 +0500

    first commit to joy voyager datatable

---
+++ resources/views/vendor/joy-voyager-datatable/bread/datatable.blade.php
@@ -0,0 +1,190 @@
+@extends('voyager::master')
+
+@section('page_title', __('voyager::generic.viewing').' '.$dataType->getTranslatedAttribute('display_name_plural'))
+
+@section('page_header')
+    <div class="container-fluid">
+        <h1 class="page-title">
+            <i class="{{ $dataType->icon }}"></i> {{ $dataType->getTranslatedAttribute('display_name_plural') }}
+        </h1>
+        @can('add', app($dataType->model_name))
+            <a href="{{ route('voyager.'.$dataType->slug.'.create') }}" class="btn btn-success btn-add-new">
+                <i class="voyager-plus"></i> <span>{{ __('voyager::generic.add_new') }}</span>
+            </a>
+        @endcan
+        @can('delete', app($dataType->model_name))
+            @include('voyager::partials.bulk-delete')
+        @endcan
+        @can('edit', app($dataType->model_name))
+            @if(!empty($dataType->order_column) && !empty($dataType->order_display_column))
+                <a href="{{ route('voyager.'.$dataType->slug.'.order') }}" class="btn btn-primary btn-add-new">
+                    <i class="voyager-list"></i> <span>{{ __('voyager::bread.order') }}</span>
+                </a>
+            @endif
+        @endcan
+        @can('delete', app($dataType->model_name))
+            @if($usesSoftDeletes)
+                <input type="checkbox" @if ($showSoftDeleted) checked @endif id="show_soft_deletes" data-toggle="toggle" data-on="{{ __('voyager::bread.soft_deletes_off') }}" data-off="{{ __('voyager::bread.soft_deletes_on') }}">
+            @endif
+        @endcan
+        @foreach($actions as $action)
+            @if (method_exists($action, 'massAction'))
+                @include('voyager::bread.partials.actions', ['action' => $action, 'data' => null])
+            @endif
+        @endforeach
+        @include('voyager::multilingual.language-selector')
+    </div>
+@stop
+
+@section('content')
+    <div class="page-content browse container-fluid">
+        @include('voyager::alerts')
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-bordered">
+                    <div class="panel-body">
+                        <div class="table-responsive">
+                            <table id="dataTable" class="table table-hover table-responsive">
+                                <thead>
+                                    <tr>
+                                        @if($showCheckboxColumn)
+                                            <th class="dt-not-orderable">
+                                                <input type="checkbox" class="select_all">
+                                            </th>
+                                        @endif
+                                        @foreach($dataType->browseRows as $row)
+                                        <th>
+                                            {{ $row->getTranslatedAttribute('display_name') }}
+                                        </th>
+                                        @endforeach
+                                        <th class="actions text-right dt-not-orderable">{{ __('voyager::generic.actions') }}</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Single delete modal --}}
+    <div class="modal modal-danger fade" tabindex="-1" id="delete_modal" role="dialog">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-label="{{ __('voyager::generic.close') }}"><span aria-hidden="true">&times;</span></button>
+                    <h4 class="modal-title"><i class="voyager-trash"></i> {{ __('voyager::generic.delete_question') }} {{ strtolower($dataType->getTranslatedAttribute('display_name_singular')) }}?</h4>
+                </div>
+                <div class="modal-footer">
+                    <form action="#" id="delete_form" method="POST">
+                        {{ method_field('DELETE') }}
+                        {{ csrf_field() }}
+                        <input type="submit" class="btn btn-danger pull-right delete-confirm" value="{{ __('voyager::generic.delete_confirm') }}">
+                    </form>
+                    <button type="button" class="btn btn-default pull-right" data-dismiss="modal">{{ __('voyager::generic.cancel') }}</button>
+                </div>
+            </div><!-- /.modal-content -->
+        </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+@stop
+
+@section('css')
+    @if(config('dashboard.data_tables.responsive'))
+        <link rel="stylesheet" href="{{ voyager_asset('lib/css/responsive.dataTables.min.css') }}">
+    @endif
+@stop
+
+@section('javascript')
+    <!-- DataTables -->
+    @if(config('dashboard.data_tables.responsive'))
+        <script src="{{ voyager_asset('lib/js/dataTables.responsive.min.js') }}"></script>
+    @endif
+    <script>
+        $(document).ready(function () {
+
+            var options = {!! json_encode(
+                array_merge([
+                    "order" => $orderColumn,
+                    "language" => __('voyager::datatable'),
+                    "columnDefs" => [
+                        ['targets' => 'dt-not-orderable', 'searchable' =>  false, 'orderable' => false],
+                    ],
+                    "processing" => true,
+                    "serverSide" => true,
+                    "ajax" => route('voyager.'.$dataType->slug.'.ajax', ['showSoftDeleted' => $showSoftDeleted]),
+                    "columns" => \dataTypeTableColumns($dataType, $showCheckboxColumn),
+                ],
+                config('voyager.dashboard.data_tables', []))
+            , true) !!};
+            
+            options = $.extend(
+                options,
+                {
+                    "drawCallback": function( settings ) {
+                        $('.select_all').off('click');
+                        $('.select_all').on('click', function(e) {
+                            console.log('clicked');
+                            e.stopPropagation();
+                            $('input[name="row_id"]').prop('checked', $(this).prop('checked')).trigger('change');
+                        });
+                    }
+                }
+            );
+
+            var table = $('#dataTable').DataTable(options);
+
+            @if ($isModelTranslatable)
+                $('.side-body').multilingual();
+                //Reinitialise the multilingual features when they change tab
+                $('#dataTable').on('draw.dt', function(){
+                    $('.side-body').data('multilingual').init();
+                })
+            @endif
+            $('.select_all').off('click');
+            $('.select_all').on('click', function(e) {
+                console.log('clicked');
+                e.stopPropagation();
+                $('input[name="row_id"]').prop('checked', $(this).prop('checked')).trigger('change');
+            });
+        });
+
+
+        var deleteFormAction;
+        $('#dataTable').on('click', 'td .delete', function (e) {
+            $('#delete_form')[0].action = '{{ route('voyager.'.$dataType->slug.'.destroy', '__id') }}'.replace('__id', $(this).data('id'));
+            $('#delete_modal').modal('show');
+        });
+
+        @if($usesSoftDeletes)
+            @php
+                $params = [
+                    'order_by' => $orderBy,
+                    'sort_order' => $sortOrder,
+                ];
+            @endphp
+            $(function() {
+                $('#show_soft_deletes').change(function() {
+                    if ($(this).prop('checked')) {
+                        $('#dataTable').before('<a id="redir" href="{{ (route('voyager.'.$dataType->slug.'.index', array_merge($params, ['showSoftDeleted' => 1]), true)) }}"></a>');
+                    }else{
+                        $('#dataTable').before('<a id="redir" href="{{ (route('voyager.'.$dataType->slug.'.index', array_merge($params, ['showSoftDeleted' => 0]), true)) }}"></a>');
+                    }
+
+                    $('#redir')[0].click();
+                })
+            })
+        @endif
+        $('#dataTable').on('change', 'input[name="row_id"]', function (e) {
+            var ids = [];
+            $('input[name="row_id"]').each(function() {
+                if ($(this).is(':checked')) {
+                    ids.push($(this).val());
+                }
+            });
+            $('.selected_ids').val(ids);
+        });
+    </script>
+@stop
